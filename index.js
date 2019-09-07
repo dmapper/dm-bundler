7146a5bbbfeedbc61944513052f17c153cc89bc2
Pavel Zhukov <cray0000@gmail.com>
2019-09-07 14:46:32 +0300

    export metro config

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const { getJsxRule } = require('./helpers')
 exports.babelConfig = require('./babel.config.js')
 exports.webpackServerConfig = require('./webpack.server')
 exports.webpackWebConfig = require('./webpack.web')
+exports.metroConfig = require('./metro.config')
 exports.getJsxRule = getJsxRule
