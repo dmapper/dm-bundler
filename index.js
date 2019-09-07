ee57dd96c01ef7661e042334a88baeb638d470e4
Pavel Zhukov <cray0000@gmail.com>
2019-09-07 14:56:47 +0300

    rename webpack configs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { getJsxRule } = require('./helpers')
 
-exports.babelConfig = require('./babel.config.js')
-exports.webpackServerConfig = require('./webpack.server')
-exports.webpackWebConfig = require('./webpack.web')
+exports.babelConfig = require('./babel.config')
+exports.webpackServerConfig = require('./webpack.server.config')
+exports.webpackWebConfig = require('./webpack.web.config')
 exports.metroConfig = require('./metro.config')
 exports.getJsxRule = getJsxRule
